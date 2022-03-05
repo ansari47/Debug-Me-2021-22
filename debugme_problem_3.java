ea6200f9e7a8c2ecb17dc5fbcfc6a7bc1a4d481e
ansari47 <85730491+ansari47@users.noreply.github.com>
2022-03-05 13:41:59 +0530

    Update debugme_problem_3.java

--- debugme_problem_3.java
+++ debugme_problem_3.java
@@ -1,19 +1,18 @@
 /*
     Problem 3
-
     Expected Output :
             [hello,
              , world]
 */
-
+import java.util.List;
 public class Problem4 {
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
 
-        List<string> list = new List<string>();
-        list.append("hello");
-        list.append("/n");
-        list.append("world");
+        List<String> list = new List<String>();
+        list.add("hello,");
+        list.add("/n");
+        list.add(", world");
 
         System.out.printf(list.toString());
 
